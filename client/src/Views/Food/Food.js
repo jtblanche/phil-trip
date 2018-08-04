85cbd82a818957cb30df5bf7578bb8dc11847f6b
John Blanche <john.t.blanche@gmail.com>
2018-08-03 20:19:22 -0700

    imageUploader

--- client/src/Views/Food/Food.js
+++ client/src/Views/Food/Food.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Food.css';
+import ImageUploader from '../../Components/ImageUploader';
 export default () => (
     <div>
+        <ImageUploader id="flbljl" />
     </div>
 );
